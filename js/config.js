6e613947e8b6bf49b2c36a0131bc4ca9ac34fa9d
Mubvafhi Moses <111067045+KillMonga130@users.noreply.github.com>
2025-05-29 23:00:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_2I9MKRay7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7gc72q4qd9r4de60sqmn56eoks', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
