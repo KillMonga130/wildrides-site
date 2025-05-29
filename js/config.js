b12fab0c692ac085fcfe1ba147bec5bba1bcd4c8
Mubvafhi Moses <111067045+KillMonga130@users.noreply.github.com>
2025-05-29 23:16:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_2I9MKRay7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7gc72q4qd9r4de60sqmn56eoks', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Oh5XDOycQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17orluq0pjsa17tofo16ebs20t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
