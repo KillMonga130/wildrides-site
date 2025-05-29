edbcbbdf5e5e4c4d428593f96bdbcaa470e9f920
Mubvafhi Moses <111067045+KillMonga130@users.noreply.github.com>
2025-05-29 23:31:36 +0200

    Update cognito-auth.js

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -53,29 +53,48 @@ var WildRydes = window.WildRydes || {};
      */
 
     function register(email, password, onSuccess, onFailure) {
-        var dataEmail = {
+        var username = toUsername(email);
+        var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute({
             Name: 'email',
             Value: email
-        };
-        var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-
-        userPool.signUp(toUsername(email), password, [attributeEmail], null,
+        });
+    
+        userPool.signUp(username, password, [attributeEmail], null,
             function signUpCallback(err, result) {
                 if (!err) {
                     onSuccess(result);
                 } else {
                     onFailure(err);
                 }
+            },
+            {
+                SecretHash: calculateSecretHash(username)
             }
         );
     }
 
+    function calculateSecretHash(username) {
+        var clientSecret = 'jtqbbugfjimopm04i2ldm9hs045npbj3o8lr7bppn15588gm66h';
+        var clientId = '17orluq0pjsa17tofo16ebs20t';
+        
+        // Message is username + clientId
+        var message = username + clientId;
+        
+        // Create HMAC SHA256 hash
+        var hmac = CryptoJS.HmacSHA256(message, clientSecret);
+        
+        // Base64 encode the hash
+        return CryptoJS.enc.Base64.stringify(hmac);
+    }
+    
     function signin(email, password, onSuccess, onFailure) {
+        var username = toUsername(email);
         var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-            Username: toUsername(email),
-            Password: password
+            Username: username,
+            Password: password,
+            SecretHash: calculateSecretHash(username)
         });
-
+        
         var cognitoUser = createCognitoUser(email);
         cognitoUser.authenticateUser(authenticationDetails, {
             onSuccess: onSuccess,
@@ -84,13 +103,19 @@ var WildRydes = window.WildRydes || {};
     }
 
     function verify(email, code, onSuccess, onFailure) {
-        createCognitoUser(email).confirmRegistration(code, true, function confirmCallback(err, result) {
-            if (!err) {
-                onSuccess(result);
-            } else {
-                onFailure(err);
+        var username = toUsername(email);
+        createCognitoUser(email).confirmRegistration(code, true, 
+            function confirmCallback(err, result) {
+                if (!err) {
+                    onSuccess(result);
+                } else {
+                    onFailure(err);
+                }
+            },
+            {
+                SecretHash: calculateSecretHash(username)
             }
-        });
+        );
     }
 
     function createCognitoUser(email) {
